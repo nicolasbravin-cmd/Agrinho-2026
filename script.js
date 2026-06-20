557f643d9a8f7de4f44cc85ac97e9506ed321b39
nicolasbravin-cmd <nicolas.bravin@escola.pr.gov.br>
2026-06-20 12:18:26 -0300

    s

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
+const botoes = document.querySelectorAll
+("button");
 
-     const botoes = document.querySelectorAll
-     ("button");
+botoes.forEach(function(botao) {
+botao.addEventListener("click", botaoClicado);
 
-     botoes.forEach(function(botao){
-          botao.addEventListener("click", botaoClicado);
-
-     }
-
-     )
-
-     function botaoClicado() {
-     console.log("fui clicado");
+function botaoClicado() {
+console.log("fui clicado");
+let texto = botao.querySelector("span");
+texto.textContent++;
 }
+});
 
-     function botaoClicado() {
-     let texto = botao.querySelector("span");
-     texto.textContent++;
-}
