2676fc24aa2254a12a1c5d9ee20456b619192d35
nicolasbravin-cmd <nicolas.bravin@escola.pr.gov.br>
2026-06-21 16:13:14 -0300

    d

--- script.js
+++ script.js
@@ -1,12 +1,45 @@
 const like = document.getElementById("like");
-const dislike = document.getElementById("dislike");
+const dislike = document.getElementById("deslike");
 
 like.addEventListener("click", () => {
     like.classList.add("ativo-like");
-    dislike.classList.remove("ativo-dislike");
+    dislike.classList.remove("ativo-deslike");
 });
 
 dislike.addEventListener("click", () => {
-    dislike.classList.add("ativo-dislike");
+    dislike.classList.add("ativo-deslike");
     like.classList.remove("ativo-like");
+});
+
+const btnLike = document.getElementById("like");
+const btnDislike = document.getElementById("deslike");
+
+const contadorLike = document.getElementById("contadorLike");
+const contadorDislike = document.getElementById("contadorDislike");
+
+let likes = 0;
+let dislikes = 0;
+
+btnLike.addEventListener("click", () => {
+
+    if (!btnLike.classList.contains("like-ativo")) {
+
+        likes++;
+        contadorLike.textContent = likes;
+
+        btnLike.classList.add("like-ativo");
+        btnDislike.classList.remove("deslike-ativo");
+    }
+});
+
+btnDislike.addEventListener("click", () => {
+
+    if (!btnDislike.classList.contains("deslike-ativo")) {
+
+        dislikes++;
+        contadorDislike.textContent = dislikes;
+
+        btnDislike.classList.add("deslike-ativo");
+        btnLike.classList.remove("like-ativo");
+    }
 });
