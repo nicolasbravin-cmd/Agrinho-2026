7d368b4e19fedfda9cd204e95689c4494ea8a339
nicolasbravin-cmd <nicolas.bravin@escola.pr.gov.br>
2026-06-23 20:21:21 -0300

    d

--- script.js
+++ script.js
@@ -1,18 +1,5 @@
-const like = document.getElementById("like");
-const dislike = document.getElementById("deslike");
-
-like.addEventListener("click", () => {
-    like.classList.add("ativo-like");
-    dislike.classList.remove("ativo-deslike");
-});
-
-dislike.addEventListener("click", () => {
-    dislike.classList.add("ativo-deslike");
-    like.classList.remove("ativo-like");
-});
-
 const btnLike = document.getElementById("like");
-const btnDislike = document.getElementById("deslike");
+const btnDislike = document.getElementById("dislike");
 
 const contadorLike = document.getElementById("contadorLike");
 const contadorDislike = document.getElementById("contadorDislike");
@@ -21,9 +8,7 @@ let likes = 0;
 let dislikes = 0;
 
 btnLike.addEventListener("click", () => {
-
     if (!btnLike.classList.contains("like-ativo")) {
-
         likes++;
         contadorLike.textContent = likes;
 
@@ -33,9 +18,7 @@ btnLike.addEventListener("click", () => {
 });
 
 btnDislike.addEventListener("click", () => {
-
     if (!btnDislike.classList.contains("deslike-ativo")) {
-
         dislikes++;
         contadorDislike.textContent = dislikes;
 
